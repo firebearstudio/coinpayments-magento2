8b1675dfb134a41ee8a9faba6e616ae0aaa6c34c
inabhani <81841709+inabhani@users.noreply.github.com>
2021-04-09 10:50:17 +0400

    Revert "update logo"

--- Model/CoinPaymentsConfigProvider.php
+++ Model/CoinPaymentsConfigProvider.php
@@ -91,7 +91,7 @@ class CoinPaymentsConfigProvider implements ConfigProviderInterface
                 'coinpayments' => [
                     'available_currencies' => $currencies,
                     'accepted_currencies' => $acceptedCurrencies,
-                    'logo' => $this->_assetRepo->getUrl('Coinpayments_CoinPayments::images/logo.svg'),
+                    'logo' => $this->_assetRepo->getUrl('Coinpayments_CoinPayments::images/logo.png'),
                     'direct_mode' => (int)$isDirect,
                     'url' => $coinpaymentsDomain,
                     'api_url' => $coinpaymentsApi
